2cf7ba26f42039955629fb86dbec51c886b9e05e
bedotrayee <55157325+bedotrayee@users.noreply.github.com>
2019-09-23 00:49:10 +0530

    Add files via upload

---
+++ t1.component.ts
@@ -0,0 +1,25 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-t1',
+  templateUrl: './t1.component.html',
+  styleUrls: ['./t1.component.css']
+})
+export class T1Component implements OnInit {
+
+  name:''; 
+address:'';
+contact:'';
+  constructor() { }
+   
+  ngOnInit() {
+  }
+  add(){
+    localStorage.setItem('name',this.name);
+    localStorage.setItem('address',this.address);
+    localStorage.setItem('contact',this.contact);
+  }
+  
+
+
+}
---
+++ t2.component.ts
@@ -0,0 +1,55 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-t2',
+  templateUrl: './t2.component.html',
+  styleUrls: ['./t2.component.css']
+})
+export class T2Component implements OnInit {
+  
+ arr=[];
+  constructor() { }
+
+  ngOnInit() {
+  }
+ 
+ 
+p=localStorage.getItem('name');
+k=localStorage.getItem('contact');
+j=localStorage.getItem('address');
+register(data: { name: string; address: string; contact: string; }){
+this.p=data.name;
+this.k=data.address;
+this.j=data.contact;
+let a=localStorage.getItem('client') || [];
+if(a!=null){
+this.arr.push(data);
+console.log(this.arr);
+localStorage.setItem('client',JSON.stringify(this.arr));
+}
+else{
+  localStorage.setItem('client',JSON.stringify(this.arr));
+}
+}
+ 
+
+}
+/*function Edit($scope) {
+  $scope.title = "Bedotrayee";
+  $scope.editor = false;
+  
+  $scope.enable = function() {
+    $scope.editor = true;
+    $scope.Title = $scope.title;
+  };
+  
+  $scope.disable = function() {
+    $scope.editor = false;
+  };
+  
+  $scope.save = function() {
+    $scope.title = $scope.Title;
+    $scope.disable();
+  };
+}
+*/
